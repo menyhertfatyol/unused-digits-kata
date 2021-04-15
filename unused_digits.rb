87180518e304beda7ea7cc05882164a6a3ede37d
mfatyol <menyhert.fatyol@gmail.com>
2021-04-15 11:30:50 +0200

    add empty project skeleton

---
+++ unused_digits.rb
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/55de6173a8fbe814ee000061
+# Given a list of integers, return the digits that are not present in any of them.
+#
+# Example:
+#
+# [12, 34, 56, 78]  =>  "09"
+# [2015, 8, 26]     =>  "3479"
+# Note: the digits in the resulting string should be sorted.
+
+class UnusedDigits
+  class << self
+    def unused_digits(*numbers)
+    end
+  end
+end
---
+++ unused_digits_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require 'rspec/expectations'
+require_relative 'unused_digits'
+
+RSpec.describe UnusedDigits do
+  describe '.unused_digits' do
+  end
+end
